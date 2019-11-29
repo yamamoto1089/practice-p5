01ecc6e81d94c8b5ed3766f3c9ab3764065255a0
ymmt1089 <ymmt1089@gmail.com>
2019-11-29 18:34:12 +0900

    P.0.2

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -3,7 +3,15 @@ function setup() {
 }
 
 function draw() {
-  ellipse(50,50,80,80);
-  point(500,50);
+
+  //唯の円
+  // ellipse(50,50,80,80); //円：左から50px,上から50px,半径80px
+  // point(50,60); //左から50px,上から60px
+
+
+  fill(128);//グレースケールの整数値
+  strokeWeight(1); //線の太さ1px
+  ellipse(40,50,60,60);
+  rect(50,50,40,20);//四角形:左から50px,上から50px,横幅40px,縦幅20px
 }
 
